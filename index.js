785355fc1dacaede7299bd82db08f80d1c200df9
calmpsycho333 <wrathking75@yahoo.com>
2021-06-06 09:34:48 -0400

    commit tree 244ba330080ce011ebe232ed1e0bc0a180f133d2

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+
+//remember that this is global scope 
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+};
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+};
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();  
+};
+
+function destructivelyRemoveLastDriver() {
+   drivers.shift();
+};
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();  
+};
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+};
+
+function appendDriver(name) {
+    const newDriver = [...drivers, name];
+    return newDriver;
+
+};
+
+function prependDriver(name) {
+    const newerDriver = [name, ...drivers];
+    return newerDriver;
+};
+
+function removeLastDriver() {
+    const removeDriver = [...drivers.slice(0,2)];
+    return removeDriver;
+
+};
+
+function removeFirstDriver() {
+    const removeFirst = [...drivers.slice(1)];
+    return removeFirst;
+}
